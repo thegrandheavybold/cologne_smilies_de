c49b20ea9d79051420f82a690e9273c95f809509
kriegsheim <mail@kriegsheim.com>
2022-10-24 17:54:13 +0200

    Initial Files

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function(eleventyConfig) {
+
+  const moment = require("moment");
+
+  //11ty PassthroughCopy
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+
+
+  // date filter (localized)
+  eleventyConfig.addNunjucksFilter("date", function (date, format, locale) {
+    locale = locale ? locale : "de";
+    moment.locale(locale);
+    return moment(date).format(format);
+  });
+
+
+  //©copyrights year output
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+
+  // You can return your Config object (optional).
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      data: "_data"
+    }
+  };
+
+};
---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: "https://cologne-smilies.de",
+  baseUrl: "/",
+  name: "Cologne Smilies",
+  metaImage: "",
+  metaTitle: "Cologne Smilies",
+  metaDescription: "Ambitioniertes Hobby-Eishockey in der Rheinischen Hockey Liga."
+};
